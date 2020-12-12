dda0f39258dd6f5bc089723b4f693e1b20aa599b
Doinakis <doinakis.michalis@gmail.com>
2020-12-12 18:45:31 +0200

    fix

--- 3rd Assignment/Subject_0.m
+++ 3rd Assignment/Subject_0.m
@@ -5,12 +5,12 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%
 clc
-clear 
-close all 
+clear
+close all
 
 syms x1 x2;
 
-% Function to be minimized 
+% Function to be minimized
 f(x1,x2) = (1/2) * x1^2 + (1/2) * x2^2;
 
 % Plot the function in the 3d space
@@ -20,4 +20,4 @@ title("3D function plot");
 xlabel("x");
 ylabel("y");
 zlabel("f(x,y)");
-saveas(model,['./Diagrams/Subject0/','function_plot.jpeg']);
+% saveas(model,['./Diagrams/Subject0/','function_plot.jpeg']);
